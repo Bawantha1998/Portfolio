3b2539a82ff0ed8def00a72f05288535db34096d
Bawantha1998 <bawanthaprampd918@outlook.com>
2024-01-30 15:43:55 +0530

    add react type

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import personImg from "../..//assets/new(1).png";
+import { ReactTyped } from "react-typed";
+
 
 const Hero = () => {
   return (
@@ -10,8 +12,21 @@ const Hero = () => {
         <div className="space-y-2 grid justify-items-center sm:justify-items-start">
          <p className="uppercase">Hello</p>
          <p className="text-4xl md:text-6xl font-bold text-black/80 dark:text-white">I'm Bawantha Pramod</p>
-         <p className="text-3xl text-gray-700 dark:text-white">Full Stack Developer</p>
-         <p className="text-black/75 dark:text-white/70 text-center sm:text-left ">Passionate Web Developer, Creative Graphic Designer and Engaging YouTube Content Creator</p>
+         {/* <p className="text-3xl text-gray-700 dark:text-white">Full Stack Developer</p> */}
+         <ReactTyped
+            className="text-3xl text-gray-700 font-bold dark:text-white"
+            strings={[
+              "Full-Stack Web Developer",
+              "Content Creator",
+              "Graphic Designer",
+              // "Portfolio Designer",
+            ]}
+            typeSpeed={150}
+            backSpeed={140}
+            loop
+          ></ReactTyped>
+
+         <p className="text-black/80 dark:text-white/70 text-center sm:text-left ">Passionate Web Developer, Creative Graphic Designer and Engaging YouTube Content Creator</p>
          <a href="mailto:bawanthapramod918@gmail.com" className="inline-block primary-btn !px-6" >Hire me</a>
         </div>
         </div>
